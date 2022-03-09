1e06974e81649f795322eb52b3617c753dd204e5
Akhila Sulgante <asulgante39@gmail.com>
2022-03-08 22:31:05 -0800

    cart items added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,15 @@
-import { Component } from "react";
+import React, { Component } from "react";
+import "../styles/Home.css";
 
 class Home extends Component {
   render() {
-    return <div>This is Home page</div>;
+    return (
+      <React.Fragment>
+        <div>
+          <></>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
--- src/components/products/Cartitems.js
+++ src/components/products/Cartitems.js
@@ -1,30 +1,93 @@
-import { Component } from "react";
+import React, { Component } from "react";
+import Popup from "../../Popup";
+
+// class Cartitems extends Component {
+//   //   constructor(props) {
+//   //     super(props);
+//   //   }
+//   state = {};
+//   render() {
+//     const { cartObj } = this.props;
+//     const { id, soapName, soapImgUrl, soapPrice, SoapDes } = cartObj;
+//     return (
+//       //cart-list?
+//       <div className="soap-list">
+//         <div>
+//           <img className="product-img" src={soapImgUrl} alt="not loading" />
+//         </div>
+
+//         <div>
+//           <p className="soap-name">{soapName}</p>
+//           <p className="soap-des">{SoapDes}</p>
+//           <h3>{soapPrice}</h3>
+//         </div>
+//         <div className="soap-cart-btn">
+//           <button className="soap-cart-text" onClick={this.addToCart} id={id}>
+//             Add to cart
+//           </button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Cartitems;
 
 class Cartitems extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //   }
-  state = {};
-  render() {
-    const { cartObj } = this.props;
-    const { id, soapName, soapImgUrl, soapPrice, SoapDes } = cartObj;
-    return (
-      //cart-list?
-      <div className="soap-list">
-        <div>
-          <img className="product-img" src={soapImgUrl} alt="not loading" />
-        </div>
+  // const [isOpen, setIsOpen] = useState(false);
 
-        <div>
-          <p className="soap-name">{soapName}</p>
-          <p className="soap-des">{SoapDes}</p>
-          <h3>{soapPrice}</h3>
-        </div>
-        <div className="soap-cart-btn">
-          <button className="soap-cart-text" onClick={this.addToCart} id={id}>
-            Add to cart
-          </button>
-        </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+    this.togglePopup = this.togglePopup.bind(this);
+  }
+
+  togglePopup() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="button"
+          value="Click to Open Popup"
+          onClick={() => this.togglePopup()}
+        />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        {this.state.isOpen && (
+          <Popup
+            content={
+              <>
+                <b>Design your Popup</b>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+                <button>Test button</button>
+              </>
+            }
+            handleClose={() => this.togglePopup()}
+          />
+        )}
       </div>
     );
   }
